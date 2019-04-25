9071e69bd69004494d27b4ce80974782d4857ebf
CodingCoke <brad61517@gmail.com>
2019-04-25 20:21:36 +0800

    hw4Eslint

--- homeworks/week2/hw4.js
+++ homeworks/week2/hw4.js
@@ -1,6 +1,6 @@
-﻿function printFactor(n) {
-  for(i = 1; i <= n ; i++){
-    if((n % i) === 0){
+function printFactor(n) {
+  for (let i = 1; i <= n; i += 1) {
+    if ((n % i) === 0) {
       console.log(i);
     }
   }
