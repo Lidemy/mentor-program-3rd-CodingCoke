2c55f7bd0a451ea4588a53dd0343c95a5f788f78
CodingCoke <brad61517@gmail.com>
2019-05-15 16:03:56 +0800

    hw2V2

--- homeworks/week4/hw2.js
+++ homeworks/week4/hw2.js
@@ -3,12 +3,18 @@ const process = require('process');
 
 if (process.argv[2] === 'list') {
   request('https://lidemy-book-store.herokuapp.com/books/', (error, response, body) => {
-    for (let i = 0; i <= 19; i += 1) {
-      console.log(`${JSON.parse(body)[i].id}${' '}${JSON.parse(body)[i].name}`);
+    const json = JSON.parse(body);
+    const numberOfBook = Object.keys(json).length;
+    for (let i = 0; i < numberOfBook; i += 1) {
+      console.log(`${json[i].id}${' '}${json[i].name}`);
     }
   });
 } else if (process.argv[2] === 'read' && process.argv[3] !== undefined) {
   request(`https://lidemy-book-store.herokuapp.com/books/${process.argv[3]}`, (error, response, body) => {
-    console.log(`${JSON.parse(body).id}${' '}${JSON.parse(body).name}`);
+    if (JSON.parse(body).id === undefined) {
+      console.log('book not found, try another id');
+    } else {
+      console.log(`${JSON.parse(body).id}${' '}${JSON.parse(body).name}`);
+    }
   });
-} else console.log('Wrong, use "list" or "read + Number" to run hw2.js');
+} else console.log('Use "list" or "read + id" to run hw2.js');
