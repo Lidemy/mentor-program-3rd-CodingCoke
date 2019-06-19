698a8de789e69865b5aa0960fce9cf0dc105de32
CodingCoke <brad61517@gmail.com>
2019-06-19 16:14:47 +0800

    hw2hw3_beta1

---
+++ homeworks/week7/hw2/script.js
@@ -0,0 +1,10 @@
+/*
+const elementQuestionnaire = document.querySelector('.questionnaire');
+const element = document.querySelector('.form_input');
+
+elementQuestionnaire.addEventListener('submit', function() {
+    if(element.value===''){
+        alert('填東西');
+    }
+});
+*/
---
+++ homeworks/week7/hw3/script.js
@@ -0,0 +1,44 @@
+/*
+let numResult = [];
+const elementResult = document.querySelector('.result');
+const num1 = document.querySelector('#num1');
+const num2 = document.querySelector('#num2');
+const num3 = document.querySelector('#num3');
+const num4 = document.querySelector('#num4');
+const num5 = document.querySelector('#num5');
+const num6 = document.querySelector('#num6');
+
+
+elementResult.innerText="123";
+
+num1.addEventListener('click', function() {
+    numResult.push(1)
+    elementResult.innerText=numResult.join('');
+})
+
+num2.addEventListener('click', function() {
+    numResult.push(2)
+    elementResult.innerText=numResult.join('');
+})
+
+num3.addEventListener('click', function() {
+    numResult.push(3)
+    elementResult.innerText=numResult.join('');
+})
+num4.addEventListener('click', function() {
+    numResult.push(4)
+    elementResult.innerText=numResult.join('');
+})
+
+num5.addEventListener('click', function() {
+    numResult.push(5)
+    elementResult.innerText=numResult.join('');
+})
+
+num6.addEventListener('click', function() {
+    numResult.push(6)
+    elementResult.innerText=numResult.join('');
+})
+
+elementResult.innerText="123";
+*/
