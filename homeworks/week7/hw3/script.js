9a97863a71dda4b3759a423db2dd89100f935e1a
CodingCoke <brad61517@gmail.com>
2019-06-24 00:32:55 +0800

    hw2hw4hw4_V1

--- homeworks/week7/hw3/script.js
+++ homeworks/week7/hw3/script.js
@@ -1,44 +1,65 @@
-/*
 let numResult = [];
+let operatorPress = [];
+let firstNumber = 0;
+let secondNumber = 0;
 const elementResult = document.querySelector('.result');
-const num1 = document.querySelector('#num1');
-const num2 = document.querySelector('#num2');
-const num3 = document.querySelector('#num3');
-const num4 = document.querySelector('#num4');
-const num5 = document.querySelector('#num5');
-const num6 = document.querySelector('#num6');
+const plus = document.querySelector('#plus');
+const minus = document.querySelector('#minus');
+const equal = document.querySelector('#equal');
+const allClear = document.querySelector('#allClear');
+const html = document.querySelector('html');
 
+function reset() {
+  numResult = [];
+  operatorPress = [];
+  firstNumber = 0;
+  secondNumber = 0;
+  elementResult.innerText = 0;
+}
 
-elementResult.innerText="123";
+html.addEventListener('click', (e) => {
+  if (e.target.id[3] < 10 && numResult[0] !== 0) {
+    if (operatorPress[0] === '=') {
+      reset();
+      operatorPress = [];
+    }
+    numResult.push(e.target.id[3]);
+    elementResult.innerText = (numResult.join(''));
+  }
+});
 
-num1.addEventListener('click', function() {
-    numResult.push(1)
-    elementResult.innerText=numResult.join('');
-})
+plus.addEventListener('click', () => {
+  operatorPress.push('+');
+  for (let i = 0; i < numResult.length; i += 1) {
+    firstNumber += numResult[i] * (10 ** ((numResult.length - 1) - i));
+  }
+  numResult = ['+ '];
+});
 
-num2.addEventListener('click', function() {
-    numResult.push(2)
-    elementResult.innerText=numResult.join('');
-})
+minus.addEventListener('click', () => {
+  operatorPress.push('－');
+  for (let i = 0; i < numResult.length; i += 1) {
+    firstNumber += numResult[i] * (10 ** ((numResult.length - 1) - i));
+  }
+  numResult = ['－ '];
+});
 
-num3.addEventListener('click', function() {
-    numResult.push(3)
-    elementResult.innerText=numResult.join('');
-})
-num4.addEventListener('click', function() {
-    numResult.push(4)
-    elementResult.innerText=numResult.join('');
-})
+equal.addEventListener('click', () => {
+  for (let i = 1; i < numResult.length; i += 1) {
+    secondNumber += numResult[i] * (10 ** ((numResult.length - 1) - i));
+  }
+  numResult = [];
+  if (operatorPress[0] === '+') {
+    numResult.push(firstNumber + secondNumber);
+    elementResult.innerText = numResult;
+  } else if (operatorPress[0] === '－') {
+    numResult.push(firstNumber - secondNumber);
+    elementResult.innerText = numResult;
+  }
+  firstNumber = 0;
+  secondNumber = 0;
+  operatorPress = [];
+  operatorPress.push('=');
+});
 
-num5.addEventListener('click', function() {
-    numResult.push(5)
-    elementResult.innerText=numResult.join('');
-})
-
-num6.addEventListener('click', function() {
-    numResult.push(6)
-    elementResult.innerText=numResult.join('');
-})
-
-elementResult.innerText="123";
-*/
+allClear.addEventListener('click', reset);
